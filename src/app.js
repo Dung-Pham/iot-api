fb637cc086ebb27a011417548995664d550414d2
Dung-Pham <phamdunglili04012003@li04012003gmail.12003gmailcom>
2024-10-26 21:23:23 +0700

    init app

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app =express()
+
+const hostname = 'localhost'
+
+const port = 8080
+
+app.get('/', function(req, res) {
+    res.send('Hello World')
+})
+
+app.listen(port, hostname, () =>{
+    console.log(`Welcome to my iot-api server, running at http://${hostname}:${port}`)
+})
